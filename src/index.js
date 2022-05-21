08bd12a1f5ff3977e32f39585d2e9f60bebebea5
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-05-21 20:22:33 +0300

    add-first-interface

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import countryCardTpl from './templates/country-card.hbs';
+import getRefs from './js/refs';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = getRefs();
+
+refs.inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  const formValue = e.target.value.trim();
+
+  fetchCountries(formValue)
+    .then(renderCountryCard)
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function renderCountryCard(country) {
+  const markup = countryCardTpl(country);
+  refs.infoDivEl.innerHTML = markup;
+}
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default function getRefs() {
+  return {
+    inputEl: document.querySelector('#search-box'),
+    listEl: document.querySelector('.country-list'),
+    infoDivEl: document.querySelector('.country-info'),
+  };
+}
